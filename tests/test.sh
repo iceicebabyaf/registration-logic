d28c3f7318f755145e422a26b4fd5eb88ed1536c
iceicebabyaf <max.nonkin@bk.ru>
2025-05-08 14:42:18 +0700

    check db test

--- tests/test.sh
+++ tests/test.sh
@@ -28,6 +28,14 @@ if [ "$UPDATE_BALANCE" != "200"]; then
     exit 1
 fi
 
+GET_DATA=$(curl curl "http://127.0.0.1:8000/get_users_db" \
+  -H "accept: application/json" \
+  -w "%{http_code}\n" -o /dev/null)
+if [ "$GET_DATA" != "200"]; then
+    echo "Geting database failed with status $GET_DATA"
+    exit 1
+fi
+
 LOGAUT_STATUS=$(curl "http://127.0.0.1:8000/user_logout?email=test@example.com" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
@@ -35,3 +43,4 @@ if [ "$LOGAUT_STATUS" != "200" ]; then
     echo "Logout failed with status $LOGAUT_STATUS"
     exit 1
 fi
+
