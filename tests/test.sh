cd5a12157e68905fb5071d8f955328f4303da909
iceicebabyaf <max.nonkin@bk.ru>
2025-05-28 13:56:18 +0700

    back to 127 port

--- tests/test.sh
+++ tests/test.sh
@@ -1,53 +1,7 @@
-# #!/bin/bash
-
-# # Тест регистрации
-# REG_STATUS=$(curl -X POST "http://127.0.0.1:8000/user_registration?email=test@example.com&password=testpass" \
-#   -H "accept: application/json" \
-#   -w "%{http_code}\n" -o /dev/null)
-# if [ "$REG_STATUS" != "200" ]; then
-#   echo "Registration failed with status $REG_STATUS"
-#   exit 1
-# fi
-
-# # Тест входа
-# LOGIN_STATUS=$(curl -X POST "http://127.0.0.1:8000/user_login?email=test@example.com&password=testpass" \
-#   -H "accept: application/json" \
-#   -w "%{http_code}\n" -o /dev/null)
-# if [ "$LOGIN_STATUS" != "200" ]; then
-#   echo "Login failed with status $LOGIN_STATUS"
-#   exit 1
-# fi
-
-
-
-# UPDATE_BALANCE=$(curl  "http://127.0.0.1:8000/update_balance?email=test@example.com&amount=100.0" \
-#   -H "accept: application/json" \
-#   -w "%{http_code}\n" -o /dev/null)
-# if [ "$UPDATE_BALANCE" != "200"]; then
-#     echo "Update balance failed with status $UPDATE_BALANCE"
-#     exit 1
-# fi
-
-# GET_DATA=$(curl curl "http://127.0.0.1:8000/get_users_db" \
-#   -H "accept: application/json" \
-#   -w "%{http_code}\n" -o /dev/null)
-# if [ "$GET_DATA" != "200"]; then
-#     echo "Geting database failed with status $GET_DATA"
-#     exit 1
-# fi
-
-# LOGAUT_STATUS=$(curl "http://127.0.0.1:8000/user_logout?email=test@example.com" \
-#   -H "accept: application/json" \
-#   -w "%{http_code}\n" -o /dev/null)
-# if [ "$LOGAUT_STATUS" != "200" ]; then
-#     echo "Logout failed with status $LOGAUT_STATUS"
-#     exit 1
-# fi
-
 #!/bin/bash
 
 # Тест регистрации
-REG_STATUS=$(curl -X POST "http://fastapi_app:8000/user_registration?email=test@example.com&password=testpass" \
+REG_STATUS=$(curl -X POST "http://127.0.0.1:8000/user_registration?email=test@example.com&password=testpass" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
 if [ "$REG_STATUS" != "200" ]; then
@@ -56,7 +10,7 @@ if [ "$REG_STATUS" != "200" ]; then
 fi
 
 # Тест входа
-LOGIN_STATUS=$(curl -X POST "http://fastapi_app:8000/user_login?email=test@example.com&password=testpass" \
+LOGIN_STATUS=$(curl -X POST "http://127.0.0.1:8000/user_login?email=test@example.com&password=testpass" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
 if [ "$LOGIN_STATUS" != "200" ]; then
@@ -65,7 +19,7 @@ if [ "$LOGIN_STATUS" != "200" ]; then
 fi
 
 # Тест обновления баланса
-UPDATE_BALANCE=$(curl -X POST "http://fastapi_app:8000/update_balance?email=test@example.com&amount=100.0" \
+UPDATE_BALANCE=$(curl -X POST "http://127.0.0.1:8000/update_balance?email=test@example.com&amount=100.0" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
 if [ "$UPDATE_BALANCE" != "200" ]; then
@@ -74,7 +28,7 @@ if [ "$UPDATE_BALANCE" != "200" ]; then
 fi
 
 # Тест получения данных
-GET_DATA=$(curl -X GET "http://fastapi_app:8000/get_users_db" \
+GET_DATA=$(curl -X GET "http://127.0.0.1:8000/get_users_db" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
 if [ "$GET_DATA" != "200" ]; then
@@ -83,7 +37,7 @@ if [ "$GET_DATA" != "200" ]; then
 fi
 
 # Тест выхода
-LOGOUT_STATUS=$(curl -X POST "http://fastapi_app:8000/user_logout?email=test@example.com" \
+LOGOUT_STATUS=$(curl -X POST "http://127.0.0.1:8000/user_logout?email=test@example.com" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
 if [ "$LOGOUT_STATUS" != "200" ]; then
