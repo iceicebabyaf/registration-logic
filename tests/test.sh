1018eb42a5dcfc566c302942720a05cfe219a12a
iceicebabyaf <max.nonkin@bk.ru>
2025-05-28 13:59:38 +0700

    back to 127 port + get to curl

--- tests/test.sh
+++ tests/test.sh
@@ -18,31 +18,29 @@ if [ "$LOGIN_STATUS" != "200" ]; then
   exit 1
 fi
 
-# Тест обновления баланса
-UPDATE_BALANCE=$(curl -X POST "http://127.0.0.1:8000/update_balance?email=test@example.com&amount=100.0" \
+
+
+UPDATE_BALANCE=$(curl  "http://127.0.0.1:8000/update_balance?email=test@example.com&amount=100.0" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
-if [ "$UPDATE_BALANCE" != "200" ]; then
-  echo "Update balance failed with status $UPDATE_BALANCE"
-  exit 1
+if [ "$UPDATE_BALANCE" != "200"]; then
+    echo "Update balance failed with status $UPDATE_BALANCE"
+    exit 1
 fi
 
-# Тест получения данных
-GET_DATA=$(curl -X GET "http://127.0.0.1:8000/get_users_db" \
+GET_DATA=$(curl "http://127.0.0.1:8000/get_users_db" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
-if [ "$GET_DATA" != "200" ]; then
-  echo "Getting database failed with status $GET_DATA"
-  exit 1
+if [ "$GET_DATA" != "200"]; then
+    echo "Geting database failed with status $GET_DATA"
+    exit 1
 fi
 
-# Тест выхода
-LOGOUT_STATUS=$(curl -X POST "http://127.0.0.1:8000/user_logout?email=test@example.com" \
+LOGAUT_STATUS=$(curl "http://127.0.0.1:8000/user_logout?email=test@example.com" \
   -H "accept: application/json" \
   -w "%{http_code}\n" -o /dev/null)
-if [ "$LOGOUT_STATUS" != "200" ]; then
-  echo "Logout failed with status $LOGOUT_STATUS"
-  exit 1
+if [ "$LOGAUT_STATUS" != "200" ]; then
+    echo "Logout failed with status $LOGAUT_STATUS"
+    exit 1
 fi
 
-echo "Tests passed"
